fbbf0a719252bd60603300849c258fdef740c612
Tom Fryers <tom.fryers@midsummerenergy.co.uk>
2025-02-20 00:14:19 +0000

    Use `--all-targets` in cargo check/clippy/test argument defaults (#86)
    * Use --all-targets in cargo check/clippy defaults
    
    This includes examples.
    
    * Use --all-targets in cargo test defaults

--- rustic-cargo.el
+++ rustic-cargo.el
@@ -111,7 +111,7 @@ instead of applying the default arguments from `rustic-default-test-arguments'."
   :type 'boolean
   :group 'rustic-cargo)
 
-(defcustom rustic-default-test-arguments "--benches --tests --all-features"
+(defcustom rustic-default-test-arguments "--all-targets --all-features"
   "Default arguments when running `cargo test'."
   :type 'string
   :group 'rustic-cargo)
@@ -121,7 +121,7 @@ instead of applying the default arguments from `rustic-default-test-arguments'."
   :type '(list string)
   :group 'rustic-cargo)
 
-(defcustom rustic-cargo-check-arguments "--benches --tests --all-features"
+(defcustom rustic-cargo-check-arguments "--all-targets --all-features"
   "Default arguments when running `cargo check'."
   :type 'string
   :group 'rustic-cargo)
